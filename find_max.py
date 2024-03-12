516ee52f96ca28d01776e82e56bb8db72afeded7
usbinn <kidbean9999@gmail.com>
2024-03-12 16:16:09 +0900

    Update find_max.py

--- find_max.py
+++ find_max.py
@@ -1,5 +1,12 @@
 def find_max_recursive(num_list):
-  max_value 
+  max_value = None
+  num = num_list.pop()
+  print(num_list)
+  if max_value is None or num > max_value : max_value = num
+  if num_list == []:
+    return max_value
+  else:
+    return find_max_recursive(num_list)
 
 
 def find_max_iterative(num_list):
@@ -8,5 +15,6 @@ def find_max_iterative(num_list):
     if max_value is None or i > max_value : max_value = i
   return max_value
 
-num_list = [1,3,4,5,2,23]
-print(find_max_iterative(num_list))
+num_list = [10,30,14,51,21,33]
+# print(find_max_iterative(num_list))
+print(find_max_recursive(num_list))
